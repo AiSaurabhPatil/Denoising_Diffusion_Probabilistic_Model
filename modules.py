e4eecf663db2e9de13db383b3627855a1109f570
AiSaurabhpatil <aientrepreneur15@gmail.com>
2023-09-22 14:13:36 +0530

    fixed minor bugs

--- modules.py
+++ modules.py
@@ -98,7 +98,7 @@ class Up(nn.Module):
         return x + emb
 
 
-class UNet(nn.Module):
+class Unet(nn.Module):
     def __init__(self, c_in=3, c_out=3, time_dim=256, device="cuda"):
         super().__init__()
         self.device = device
